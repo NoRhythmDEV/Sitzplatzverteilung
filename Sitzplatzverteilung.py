66300be49e16232a37ae3e61239f324e0d240754
NoRhythmDEV <norhythm.official@gmail.com>
2023-10-18 19:35:59 +0200

    added timestamp, fixed bug

--- Sitzplatzverteilung.py
+++ Sitzplatzverteilung.py
@@ -4,6 +4,7 @@ from reportlab.pdfgen import canvas
 import openpyxl
 import os
 import random
+import time
 
 # Funktion zum Importieren von Daten aus einer Excel-Datei
 def import_from_excel():
@@ -25,11 +26,24 @@ def import_from_excel():
 # Funktion zum Speichern als PDF
 def save_as_pdf():
     desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
-    file_path = os.path.join(desktop_path, "Sitzordnung.pdf")
+    
+    # Get the current timestamp in the format HHMMSS
+    timestr = time.strftime("%H-%M-%S")
+
+    # Original filename
+    original_filename = "Sitzordnung.pdf"
+
+    # Extract the filename and extension
+    filename, extension = os.path.splitext(original_filename)
+
+    # Append the timestamp to the filename
+    new_filename = f"{filename}_{timestr}{extension}"
+
+    file_path = os.path.join(desktop_path, new_filename)
     text_content = text_output.get("1.0", tk.END).strip()
 
     pdf = canvas.Canvas(file_path)
-    pdf.setFont("Helvetica", 12)
+    pdf.setFont("Helvetica", 10)
     pdf.drawString(50, 750, "Sitzordnungen:")
 
     y_position = 730
@@ -39,28 +53,50 @@ def save_as_pdf():
 
     pdf.save()
 
-    messagebox.showinfo("PDF gespeichert", f"Die Datei wurde als 'Sitzordnung.pdf' auf dem Desktop gespeichert:\n{file_path}")
+    messagebox.showinfo("PDF gespeichert", f"Die Datei wurde als '{new_filename}' auf dem Desktop gespeichert:\n{file_path}")
 
 # Funktion zum Generieren der Sitzordnungen
 def generate_sitzordnungen():
     namen_liste = entry_namen.get("1.0", tk.END).strip()
     personen_pro_tisch = int(entry_personen_pro_tisch.get())
 
-    namen = [name.strip() for name in namen_liste.split(',') if name.strip()]
+    # Namen in eine Liste aufteilen und bereinigen
+    namen = [name.strip() for name in namen_liste.split(',')]
 
+    # Zufällige Sitzordnung generieren
     random.shuffle(namen)
 
+    # Anzahl der Tische berechnen
     anzahl_tische = len(namen) // personen_pro_tisch
+    restliche_personen = len(namen) % personen_pro_tisch
 
+    # Output-Text für beide Sitzordnungen
     sitzordnungen_text = ""
-    for i in range(2):
-        sitzordnungen_text += f"Sitzordnung {i+1}:\n"
-        for j in range(anzahl_tische):
-            tisch_nummer = j + 1
-            personen_am_tisch = namen[j * personen_pro_tisch: (j + 1) * personen_pro_tisch]
-            sitzordnungen_text += f"Tisch {tisch_nummer} → [ {' | '.join(personen_am_tisch)} ]\n"
-        sitzordnungen_text += "\n"
 
+    for ordnung_nummer in range(2):
+        sitzordnungen_text += f"Sitzordnung {ordnung_nummer + 1}:\n"
+        tisch_counter = 1
+        personen_counter = 0
+
+        for i in range(anzahl_tische):
+            tisch_personen = []
+            for j in range(personen_pro_tisch):
+                tisch_personen.append(namen[personen_counter])
+                personen_counter += 1
+
+            sitzordnungen_text += f"[Tisch {tisch_counter}] → [{', '.join(tisch_personen)}]\n"
+            tisch_counter += 1
+
+        # Restliche Personen hinzufügen
+        if restliche_personen > 0:
+            tisch_personen = []
+            for k in range(restliche_personen):
+                tisch_personen.append(namen[personen_counter])
+                personen_counter += 1
+
+            sitzordnungen_text += f"[Tisch {tisch_counter}] → [{', '.join(tisch_personen)}]\n\n"
+
+    # Output im Textfeld anzeigen
     text_output.delete("1.0", tk.END)
     text_output.insert(tk.END, sitzordnungen_text)
 
@@ -88,7 +124,7 @@ entry_namen.grid(row=1, column=0, padx=(15, 10), pady=(0, 10), sticky="nsew")
 label_ausgabe = tk.Label(root, font=("Arial", 16), text="Ausgabe:")
 label_ausgabe.grid(row=0, column=1, padx=(15, 10), pady=(15, 10), sticky="w",)
 
-text_output = tk.Text(root, font=("Arial", 12), width=60, height=40)
+text_output = tk.Text(root, font=("Arial", 12), width=120, height=40)
 text_output.grid(row=1, column=1, padx=(15, 10), pady=(0, 10), sticky="nsew")
 
 # Eingabefeld für Personen pro Tisch
@@ -116,5 +152,3 @@ exit_button.grid(row=5, column=1, padx=(15, 10), pady=(10, 15))
 
 # GUI starten
 root.mainloop()
-
-# test
